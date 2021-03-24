0403a4c03b318980fd4f5e2b411c9aa2d2dc054d
gustavoluppi <gustavoluppi@id.uff.br>
2021-03-23 22:51:15 -0300

    continuando a tela de busca e criação do componente de perfil

--- lib/screens/Search_Oponents.dart
+++ lib/screens/Search_Oponents.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-class Search_Oponents extends StatelessWidget{
+import 'package:jogodavelha/components/SearchResult.dart';
+
+import '../constants/Messages.dart';
+import '../constants/Messages.dart';
+import '../storage/CurrentUser.dart';
+import '../storage/CurrentUser.dart';
+import '../storage/CurrentUser.dart';
+
+class Search_Oponents extends StatelessWidget {
   //Todo: Mudar para Statefull
   //Todo: Não usar AppBar, Fazer tudo no body (O componente do bernardo tem um exemplo de row)
   // Todo: DataSearch não precisa ser uma classe
@@ -8,62 +16,34 @@ class Search_Oponents extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text("Pesquisar"),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.search),onPressed:(){
-            showSearch(context: context, delegate: DataSearch());
-          })
-        ],
-      ),
-        body: new Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            new Expanded(
-              child: new GridView.builder(
-                padding: const EdgeInsets.all(10.0),
-                gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    childAspectRatio: 1.0,
-                    crossAxisSpacing: 9.0,
-                    mainAxisSpacing: 9.0),
-                itemCount: buttonsList.length,
-                itemBuilder: (context, i) => new SizedBox(
-                  width: 100.0,
-                  height: 100.0,
-                  child: new RaisedButton(
-                    padding: const EdgeInsets.all(8.0),
-                    onPressed: buttonsList[i].enabled
-                        ? () => playGame(buttonsList[i])
-                        : null,
-                    child: new Text(
-                      buttonsList[i].text,
-                      style:
-                      new TextStyle(color: Colors.amber, fontSize: 20.0),
-                    ),
-                    color: buttonsList[i].bg,
-                    disabledColor: buttonsList[i].bg,
-                  ),
-                ),
-              ),
-            ),
-            new RaisedButton(
-              child: new Text(
-                "Reset",
-                style: new TextStyle(color: Colors.white, fontSize: 20.0),
-              ),
-              color: Colors.red,
-              padding: const EdgeInsets.all(20.0),
-              onPressed: resetGame,
-            )
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(AppMessages.searchTitle),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  showSearch(context: context, delegate: DataSearch());
+                })
           ],
-        ));
-  }
-    );
-  }
+        ),
+        body: Column(
+          //padding: EdgeInsets.only(top: 60, left: 40, right: 40),
+          children: <Widget>[
+            SizedBox(
+              height: 30.00,
+            ),
+            SearchResult(CurrentUser.user.urlImage, CurrentUser.user.nickname, CurrentUser.user.name, 4, 3,
+                AppMessages.redButtonAdd, ()=>{}, ()=>{})
+          ]),
+          //decoration: BoxDecoration(
+            //image: DecorationImage(
+              //image: AssetImage("assets/bg_gradient.jpg"),
+              //fit: BoxFit.cover,
 
+
+        );
+  }
 }
 
 class DataSearch extends SearchDelegate<String> {
@@ -73,18 +53,21 @@ class DataSearch extends SearchDelegate<String> {
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
-      IconButton(icon: Icon(Icons.clear), onPressed: () {
-        query = "";
-      })
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = "";
+          })
     ];
   }
 
   @override
   Widget buildLeading(BuildContext context) {
-    return IconButton(icon: AnimatedIcon(
-      icon: AnimatedIcons.menu_arrow,
-      progress: transitionAnimation,
-    ),
+    return IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+        ),
         onPressed: () {
           close(context, null);
         });
@@ -106,11 +89,8 @@ class DataSearch extends SearchDelegate<String> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Container(
-      child: Text('oi'),
-      color: Colors.red,
-      height: 100.00,
-      width: 395.00,
-    );
+    return SearchResult(CurrentUser.user.urlImage, CurrentUser.user.nickname, CurrentUser.user.name, 4, 3,
+        AppMessages.redButtonAdd, ()=>{}, ()=>{});
+
   }
 }
