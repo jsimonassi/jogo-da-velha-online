7b5a71105671222d243dc7d25a24afb9261d0127
João Victor Simonassi <jsimonassi@id.uff.br>
2021-04-19 22:25:59 -0300

    Corrigindo mensagens

--- lib/constants/Messages.dart
+++ lib/constants/Messages.dart
@@ -8,14 +8,14 @@ class AppMessages {
   static const String emailPlaceholder = "E-mail:";
   static const String passwordPlaceholder = "Senha:";
   static const String nickNamePlaceholder = "Nickname";
-  static const String chatPlaceholder = "Escreva sua mensagem...";
+  static const String chatPlaceholder = "Escreva sua mensagem";
   static const String searchTitle ="Buscar Amigos";
   static const String redButton ='DESAFIAR';
   static const String wins ='Vitórias:';
   static const String losses ='Derrotas:';
   static const String redButtonAdd ='ADICIONAR';
   static const String initMatch ='Iniciando Partida';
-  static const String itsYourTurn ='É a sua vez';
+  static const String itsYourTurn ='É a sua vez!';
   static const String endOfMatch ='Fim de Jogo!';
   static const String winner ='Vencedor:';
   static const String old ='Velha';
@@ -23,7 +23,7 @@ class AppMessages {
   static const String playAgain ='JOGAR NOVAMENTE';
 
   static const String initLogin = "ENTRAR";
-  static const String singUpMessage = "Não tem uma conta? Cadastre-se!";
+  static const String singUpMessage = "Não tem uma conta? Cadastre-se.";
   static const String resetPassword = "Esqueci minha senha";
   static const String resetPasswordTitle = "Redefinir senha";
   static const String emailSendBtn = "REDEFINIR SENHA";
@@ -69,8 +69,8 @@ class AppMessages {
 
   //Error Messages
   static const String inputBlank = "Este campo é obrigatório";
-  static const String invalidEmail = "E-mail inválido, tente novamente";
-  static const String invalidPassword = "Senha incorreta, tente novamente";
+  static const String invalidEmail = "E-mail inválido. Tente novamente.";
+  static const String invalidPassword = "Senha incorreta. Tente novamente.";
   static const String error = "Deu velha :(";
   static const String undefinedError = "Ocorreu um problema durante a solicitação";
   static const String undefinedUser = "Usuário não encontrado. Verifique os dados e tente novamente.";
